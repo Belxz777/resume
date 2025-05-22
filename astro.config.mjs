ef9619746790caf5ade3f2105ae773367496a071
Belxz777 <belxz888@gmail.com>
2025-05-22 16:23:53 +0400

    issue with conf does not make a sense

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,12 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    preview:{
+      host: '0.0.0.0',
+      port: 4321,
+      allowedHosts:['bell-x.ru']
+    },
+  
   }
 });
